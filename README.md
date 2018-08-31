fbf53cd2f611f98cfa2c101eadfc324325d68ba3
Youssef Sherif <sharief@aucegypt.edu>
2018-08-31 09:15:20 +0200

    Update

--- README.md
+++ README.md
@@ -1,64 +1,8 @@
-# Dialogflow Bike Shop Sample
-
-## Setup Instructions
-### Dialogflow and Fulfillment Setup
-Click on the **Add to Dialogflow** button below and follow the prompts to create a new agent:
-
-[![Bike Shop](https://storage.googleapis.com/dialogflow-oneclick/deploy.svg "Bike Shop")](https://console.dialogflow.com/api-client/oneclick?templateUrl=https://storage.googleapis.com/dialogflow-oneclick/bike-shop-agent.zip&agentName=BikeShopSample)
-
-#### Service Account Setup
-1. In [Dialogflow's console](https://console.dialogflow.com), go to settings ⚙ and under the general tab, you'll see the project ID section with a Google Cloud link to open the Google Cloud console. Open **Google Cloud**.
-1. In the Cloud console, go to the menu icon **☰ > APIs & Services > Library**
-1. Select **Google Calendar API** and then **Enable** to enable the API on your cloud project.
-1. Under the menu icon **☰ > APIs & Services > Credentials > Create Credentials > Service Account Key**.
-1. Under **Create service account key**, select **New Service Account** from the dropdown and enter `bike-shop-calendar` for the name and click **Create**. In the popup, select **Create Without Role**.
-  + JSON file will be downloaded to your computer that you will need in the setup sections below.
-
-#### Bike Shop Calendar Setup
-1. Open the JSON file that was downloaded in the previous section and copy the email address indicated by the `client_email` field
-```js
-// Ex:
-bike-shop-calendar@${PROJECTID}.iam.gserviceaccount.com
-```
-1. [Open Google Calendar](https://calendar.google.com). On the left, next to **Add a friend's calendar** click the **+** and select **New Calendar**
-1. Enter `Bike Shop` for the name of the calendar and select **Create Calendar**. Next, go to the `Bike Shop` calendar that will appear on the left column.
-1. Paste the email copied from the first step into the **Add people** field of the **Share with specific people** section and then select **Make changes to events** in the permissions dropdown and select **Send**.
-1. While still in Settings, scroll down and copy the **Calendar ID** in the **Integrate Calendar** section.
-
-#### Add Service Account and Calendar ID to Fulfillment
-1. Go to the `index.js` file in [Dialogflow's Fulfillment section](https://console.dialogflow.com/api-client/#/agent//fulfillment)
-1. Take the **Calendar ID** copied from the prior section and replace `<INSERT CALENDAR ID HERE>` on line 24 of `index.js`.
-```js
-// Ex:
-const calendarId = '6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com';
-```
-1. Next copy the contents of the JSON file downloaded in the "Service Account Setup" section and paste it into the empty object on line 25 of `index.js` `const serviceAccount = {}`.
-```js
-//Ex:
-    const serviceAccount = {
-      "type": "service_account",
-      "project_id": "bikesample",
-    ...
-  };
-```
-1. Click **Deploy** at at the bottom of the page.
-
-#### [OPTIONAL] Add Bike Shop FAQ Knowledge Connector
-1. Open Dialogflow and go to **Settings (⚙) > General > Beta features** and click the switch to "Enable beta features and APIs" and then click "Save"
-1. Click on the Knowledge tab in the left column and then "Create Knowledge base" in the top right.  Name the knowledge base `Bike Shop` and click "Save"
-1. Next, click `Create the first one.` in the center of the screen to create your first knowledge document.  Enter in `Bike Shop FAQ` for the document name, `text/csv` for the "Mime Type" and `FAQ` for the "Knowledge type".
-1.  Under "Data source" select `URL` and enter `https://raw.githubusercontent.com/dialogflow/fulfillment-bike-shop-nodejs/master/bike-shop-faq.csv` and then click "Create".
-1. After the knowledge document has been created in the response section click "Add response". In text response you should see `$Knowledge.Answer[1]`. Click "Save".
-1. Try it out! In the simulator on the right enter `do you sell ebikes?`.  You should see the response from the CSV you just uploaded: "We sell Electric bikes. We also can outfit your existing bike with a retrofit Bionx electric wheel kit. We do not do gas powered conversions."
-
-## Running the sample
-1. In [Dialogflow's console](https://console.dialogflow.com), in the Dialogflow simulator on the right, query your Dialogflow agent with `My bike is broken` and respond to the questions your Dialogflow agent asks.   After getting the required information, an appointment will be added to the "Bike Shop Calendar" calendar.
-
-## How to make contributions?
-Please read and follow the steps in the CONTRIBUTING.md.
-
-## License
-See [LICENSE](LICENSE).
-
-## Terms
-Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/).
+* Create a Dialogflow agent with an entity called AppointmentType that could be either
+    * service
+    * tune up
+* 
+* Enable Calendar Api in GCloud and create a service account key which would produce a json file with credentials
+    * Create a calendar through a google account that does not belong to an organiztion other than google (not aucegypt.edu)
+        * Get the **client-email** from this json file and share the newly craeted calendar with that email and give persmission for tis email to **Make changes to events** 
+        * Take notice of the **calendarId** because it would be used in the lambda function
--- functions/index.js
+++ functions/index.js
@@ -1,111 +1,99 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
 const functions = require('firebase-functions');
-const {google} = require('googleapis');
-const {WebhookClient} = require('dialogflow-fulfillment');
+const { google } = require('googleapis');
+const { WebhookClient } = require('dialogflow-fulfillment');
 
 // Enter your calendar ID below and service account JSON below, see https://github.com/dialogflow/bike-shop/blob/master/README.md#calendar-setup
-const calendarId = '<INSERT CALENDAR ID HERE>'; // looks like "6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com"
+const calendarId = 'urocnnrie6glhe5p8vc3t97up0@group.calendar.google.com'; // looks like "6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com"
 const serviceAccount = {}; // Starts with {"type": "service_account",...
 
 // Set up Google Calendar Service account credentials
 const serviceAccountAuth = new google.auth.JWT({
-  email: serviceAccount.client_email,
-  key: serviceAccount.private_key,
-  scopes: 'https://www.googleapis.com/auth/calendar'
+    email: serviceAccount.client_email,
+    key: serviceAccount.private_key,
+    scopes: 'https://www.googleapis.com/auth/calendar'
 });
+// const app = dialogflow({ debug: true });
 
 const calendar = google.calendar('v3');
 process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
 
 const timeZone = 'America/Los_Angeles';
 const timeZoneOffset = '-07:00';
+// exports.bikeshop = functions.https.onRequest(app);
+
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
+    const agent = new WebhookClient({ request, response });
 
-  function hours (agent) {
-    if (currentlyOpen()) {
-      agent.add(`We're open now! We close at 5pm today.`);
-    } else {
-      agent.add(`We're currently closed, but we open every weekday at 9am!`);
+    function hours(agent) {
+        if (currentlyOpen()) {
+            agent.add(`We're open now! We close at 5pm today.`);
+        } else {
+            agent.add(`We're currently closed, but we open every weekday at 9am!`);
+        }
     }
-  }
 
-  function makeAppointment (agent) {
-    // Calculate appointment start and end datetimes (end = +1hr from start)
-    const dateTimeStart = new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0] + timeZoneOffset));
-    const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
-    const appointmentTimeString = dateTimeStart.toLocaleString(
-      'en-US',
-      { month: 'long', day: 'numeric', hour: 'numeric', timeZone: timeZone }
-    );
+    function makeAppointment(agent) {
+        // Calculate appointment start and end datetimes (end = +1hr from start)
+        const dateTimeStart = new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0] + timeZoneOffset));
+        const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
+        const appointmentTimeString = dateTimeStart.toLocaleString(
+            'en-US',
+            { month: 'long', day: 'numeric', hour: 'numeric', timeZone: timeZone }
+        );
 
-    // Check the availibility of the time, and make an appointment if there is time on the calendar
-    return createCalendarEvent(dateTimeStart, dateTimeEnd).then(() => {
-      agent.add(`Ok, let me see if we can fit you in. ${appointmentTimeString} is fine!. Do you need a repair or just a tune-up?`);
-    }).catch(() => {
-      agent.add(`I'm sorry, there are no slots available for ${appointmentTimeString}.`);
-    });
-  }
+        // Check the availibility of the time, and make an appointment if there is time on the calendar
+        return createCalendarEvent(dateTimeStart, dateTimeEnd).then(() => {
+            agent.add(`Ok, let me see if we can fit you in. ${appointmentTimeString} is fine!. Do you need a repair or just a tune-up?`);
+        }).catch(() => {
+            agent.add(`I'm sorry, there are no slots available for ${appointmentTimeString}.`);
+        });
+    }
 
-  let intentMap = new Map();
-  intentMap.set('Hours', hours);
-  intentMap.set('Make Appointment', makeAppointment);
-  agent.handleRequest(intentMap);
+    let intentMap = new Map();
+    intentMap.set('Hours', hours);
+    intentMap.set('Make Appointment', makeAppointment);
+    agent.handleRequest(intentMap);
 });
 
-function currentlyOpen () {
-  // Get current datetime with proper timezone
-  let date = new Date();
-  date.setHours(date.getHours() + parseInt(timeZoneOffset.split(':')[0]));
-  date.setMinutes(date.getMinutes() + parseInt(timeZoneOffset.split(':')[0][0] + timeZoneOffset.split(':')[1]));
+function currentlyOpen() {
+    // Get current datetime with proper timezone
+    let date = new Date();
+    date.setHours(date.getHours() + parseInt(timeZoneOffset.split(':')[0]));
+    date.setMinutes(date.getMinutes() + parseInt(timeZoneOffset.split(':')[0][0] + timeZoneOffset.split(':')[1]));
 
-  return date.getDay() >= 1 &&
+    return date.getDay() >= 1 &&
         date.getDay() <= 5 &&
         date.getHours() >= 9 &&
         date.getHours() <= 17;
 }
 
-function createCalendarEvent (dateTimeStart, dateTimeEnd) {
-  return new Promise((resolve, reject) => {
-    calendar.events.list({
-      auth: serviceAccountAuth, // List events for time period
-      calendarId: calendarId,
-      timeMin: dateTimeStart.toISOString(),
-      timeMax: dateTimeEnd.toISOString()
-    }, (err, calendarResponse) => {
-      // Check if there is a event already on the Bike Shop Calendar
-      if (err || calendarResponse.data.items.length > 0) {
-        reject(err || new Error('Requested time conflicts with another appointment'));
-      } else {
-        // Create event for the requested time period
-        calendar.events.insert({ auth: serviceAccountAuth,
-          calendarId: calendarId,
-          resource: {summary: 'Bike Appointment',
-            start: {dateTime: dateTimeStart},
-            end: {dateTime: dateTimeEnd}}
-        }, (err, event) => {
-          err ? reject(err) : resolve(event);
-        }
-        );
-      }
+function createCalendarEvent(dateTimeStart, dateTimeEnd) {
+    return new Promise((resolve, reject) => {
+        calendar.events.list({
+            auth: serviceAccountAuth, // List events for time period
+            calendarId: calendarId,
+            timeMin: dateTimeStart.toISOString(),
+            timeMax: dateTimeEnd.toISOString()
+        }, (err, calendarResponse) => {
+            // Check if there is a event already on the Bike Shop Calendar
+            if (err || calendarResponse.data.items.length > 0) {
+                reject(err || new Error('Requested time conflicts with another appointment'));
+            } else {
+                // Create event for the requested time period
+                calendar.events.insert({
+                    auth: serviceAccountAuth,
+                    calendarId: calendarId,
+                    resource: {
+                        summary: 'Bike Appointment',
+                        start: { dateTime: dateTimeStart },
+                        end: { dateTime: dateTimeEnd }
+                    }
+                }, (err, event) => {
+                    err ? reject(err) : resolve(event);
+                }
+                );
+            }
+        });
     });
-  });
 }
